371591d95c45e20e15c0d2c90311455653148f83
Gabriel Antonio Rodrigues Morais <129913437+GabrielAntonioRodriguesMorais@users.noreply.github.com>
2024-12-02 16:07:24 -0300

    Update ingrediente.c

--- ingrediente.c
+++ ingrediente.c
@@ -1,58 +1,80 @@
 #include "ingrediente.h"
 
-Ingredientes* adicionar_ingrediente_CRUD(){
-    Ingredientes *ingrediente;
-    int qnt = 0;//Contador para a quantidade de ingredientes que serao cadastrados
+Ingredientes* adicionar_ingrediente_CRUD(int *qnt){//qnt = quantidade de ingredientes, deve ser inicializado na main e na hora de chamar a funcao passar o seu endereco como parametro
+    Ingredientes *ingrediente = NULL;
     char continuar;
+    FILE* arquivo = fopen("Ingredientes.txt", "a");
 
     do{
-        ingrediente = realloc(ingrediente, (qnt + 1) * sizeof(Ingredientes));
+        ingrediente = realloc(ingrediente, (*qnt + 1) * sizeof(Ingredientes));
         
         printf("Digite o ID do Ingrediente: ");
-        scanf("%d", &ingrediente[qnt].id);
+        scanf("%d", &ingrediente[*qnt].id);
         getchar();
-        printf("\n");
         
         printf("Digite o nome do Ingrediente: ");
-        fgets(ingrediente[qnt].nome, 30, stdin);
-        ingrediente[qnt].nome[strcspn(ingrediente[qnt].nome, "\n")] = '\0';
+        fgets(ingrediente[*qnt].nome, 30, stdin);
+        ingrediente[qnt].nome[strcspn(ingrediente[*qnt].nome, "\n")] = '\0';
         printf("\n");
         
-        printf("Digite o preço do Ingrediente: ");
-        scanf("%f", &ingrediente[qnt].preco);
+        printf("Digite o preco do Ingrediente: ");
+        scanf("%f", &ingrediente[*qnt].preco);
 
-        qnt++;
+         fprintf(arquivo, "%d - %s R$%.2f\n", ingrediente[*qnt].id, ingrediente[*qnt].nome, ingrediente[*qnt].preco);
+        
+        (*qnt)++;
 
-        printf("Gostaria de adicionar mais um Ingrediente? ");
+        printf("\n");
+        printf("Gostaria de adicionar mais um Ingrediente?(S ou N) ");
         scanf(" %c", &continuar);        
     }while(continuar == 'S' || continuar == 's'); 
 
+    fclose(arquivo);
     return ingredientes;
 }
-int quantidade_ingredientes_cadastrados(){
-    int quantidade = 0;
-    char linha[30];
-    
-    FILE* arquivo = fopen("ingredientes.txt", "r");
-    
-    while (fgets(Linha, sizeof(Linha), arquivo) != NULL)
-    {
-        quantidade++;
-    }
-return quantidade;
-}
-void visualizar_ingrediente_CRUD(int qnt){
-    int qnt = quantidade_ingredientes_cadastrados();
+void visualizar_ingrediente_CRUD(Ingredientes *ingrediente, int qnt){//ingredientes deve ser 
     printf("\nIngredientes cadastrados:\n");
     for(int i = 0; i < qnt; i++){
         printf("%d - %s R$%.2f\n", ingrediente[i].id, ingrediente[i].nome, ingrediente[i].preco);
     }
 }
-void editar_ingrediente_CRUD(){}
+void editar_ingrediente_CRUD(Ingredientes *ingrediente, int qnt){
+    char alteracao[10], novo_nome[30];    
+    int id_alteracao, posicao_alteracao;
+    float novo_preco = 0.0;
+    FILE* arquivo = fopen("Ingredientes.txt", "a");
+
+    visualizar_ingrediente_CRUD(Ingredientes *ingrediente, int qnt);
+    printf("Digite o ID do Ingrediente a ser alterado: \n");
+    getchar();
+
+    for(int i = 0; i < qnt; i++){
+        if(ingrediente[i].id == id_alteracao){
+            posicao_alteracao = i;
+        }
+    }
+        
+    printf("O que gostaria de alterar(Nome ou Preco)?: \n");
+    fgets(alteracao, 10, stdin);
+    alteracao[strcspn(alteracao, "\n")] = '\0';
+        
+    if(strcmp(alteracao, "nome") == 0){
+    printf("Qual o novo nome?\n");
+    fgets(novo_nome, 30, stdin);
+    novo_nome[strcspn(novo_nome, "\n")] = '\0';
+    strcpy(ingrediente[posicao_alteracao].nome, novo_nome);
+    }else if(strcmp(alteracao, "preco") == 0){
+    printf("Qual o novo preco?\n");
+    scanf("%f", &novo_preco);
+    getchar();
+    ingrediente[posicao_alteracao].preco = novo_preco;
+    }
+        
+}
 void remover_ingrediente_CRUD(){}
 
 int menu_ingrediente() {
-    int NumeroOperaçãoingrediente=0;
+    int NumeroOperaçãoingrediente = 0;
     printf("\n");
     printf("*******************************|\n");
     printf("INGREDIENTE(s):                |\n");
