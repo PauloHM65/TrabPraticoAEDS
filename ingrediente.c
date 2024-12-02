74d31a06188c5e8c592449fdce96d5d318e25c4b
Gabriel Antonio Rodrigues Morais <129913437+GabrielAntonioRodriguesMorais@users.noreply.github.com>
2024-12-02 12:40:30 -0300

    Update ingrediente.c

--- ingrediente.c
+++ ingrediente.c
@@ -1,6 +1,8 @@
 #include "ingrediente.h"
 
-void adicionar_ingrediente_CRUD(){}
+void adicionar_ingrediente_CRUD(){
+    
+}
 void visualizar_ingrediente_CRUD(){}
 void editar_ingrediente_CRUD(){}
 void remover_ingrediente_CRUD(){}
@@ -8,14 +10,14 @@ void remover_ingrediente_CRUD(){}
 int menu_ingrediente() {
     int NumeroOperaçãoingrediente=0;
     printf("\n");
-    printf("*****************************|\n");
-    printf("PIZZA(s):                    |\n");
-    printf("*****************************|\n");
-    printf(" 1 - Adicionar Pizza         |\n");
-    printf(" 2 - Visualizar Pizzas       |\n");
-    printf(" 3 - Editar Pizza            |\n");
-    printf(" 4 - Remover Pizza           |\n");
-    printf("*****************************|\n");
+    printf("*******************************|\n");
+    printf("INGREDIENTE(s):                |\n");
+    printf("*******************************|\n");
+    printf(" 1 - Adicionar Ingrediente(s)  |\n");
+    printf(" 2 - Visualizar Ingrediente(s) |\n");
+    printf(" 3 - Editar Ingrediente(s)     |\n");
+    printf(" 4 - Remover Ingrediente(s)    |\n");
+    printf("*******************************|\n");
     scanf("%d",&NumeroOperaçãoingrediente);
     if (!(NumeroOperaçãoingrediente == 1 || NumeroOperaçãoingrediente == 2 || NumeroOperaçãoingrediente == 3|| NumeroOperaçãoingrediente == 4)) {
         NumeroOperaçãoingrediente = 0;
